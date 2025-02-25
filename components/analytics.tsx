92188d15f3565b2787339efdeb3f539a09b6434a
kaushiktak19 <kaushiktak19@gmail.com>
2025-02-25 16:05:23 +0530

    fixed suspense error while build

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,23 +1,29 @@
-"use client"
+"use client";
 
-import { usePathname, useSearchParams } from "next/navigation"
-import Script from "next/script"
-import { useEffect } from "react"
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+import { useEffect, Suspense } from "react";
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
 
 export function Analytics() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (typeof window.gtag !== "undefined") {
       window.gtag("config", "G-XXXXXXXXXX", {
         page_path: pathname + searchParams.toString(),
-      })
+      });
     }
-  }, [pathname, searchParams])
+  }, [pathname, searchParams]);
 
   return (
-    <>
+    <Suspense fallback={null}>
       <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`} />
       <Script
         id="google-analytics"
@@ -31,7 +37,6 @@ export function Analytics() {
           `,
         }}
       />
-    </>
-  )
+    </Suspense>
+  );
 }
-
